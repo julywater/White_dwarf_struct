c2980f2961a832ee63a2373dc19a5190937656d0
Yike Tang <tangyike455665@gmail.com>
2014-11-26 16:09:39 -0500

    Update creat_WD.cpp

--- creat_WD.cpp
+++ creat_WD.cpp
@@ -1,10 +1,16 @@
 #include<iostream>
+#include <string>
+#include <iterator>
+#include <algorithm>
+#include <array>
+#include <math.h>
+#define M_PI 3.1415926
 using namespace std;
 class white_dwarf{
   private:
     int N; //number of bins
-    rho_array;
-    pres_array;
+    array<double,N> rho_array;
+    array<double,N> pres_array;
     const double Radius;
     const double Mass;
     // inital physical consts radius and total mass
@@ -14,12 +20,27 @@ class white_dwarf{
     double vel_cof;
     double dr;
   public:
-    white_dwarf(double n=1.0/3,Radius=,Mass=){
-     rho_array=;
-     pres_array=;
-     dr=;
-     density_array=;
-     pres_cof=;
-     vel_cof=;
+    white_dwarf(const Gamma=5.0/3,Radius=,Mass=){
+      const double n=1.0/(Gamma-1);
+      array<double,N> ksi;
+      const double alpha=sqrt((n+1)/4/M_PI);
+      const double dksi=3.5/M;
+      dr=dksi*alpha;
+      for(int i=0;i<N;i++)
+        ksi[i]=i*dksi;
+      rho_array[0]=1.0;
+      rho_array[1]=1.0;
+      for(i=2;i<N;i++){
+        rho_array[i]=(-pow(rho_array[i-1],n)-(rho_array[i-2]-2*rho_array[i-1])/dksi/dksi+rho_array[i-2]/ksi[i-1]/dksi)/(1.0/dksi/dksi+1.0/(dksi*ksi[i-1]));
+        if(rho_array[i]<=0)
+          rho_array[i]=0;
+      }
+        for(i=0;i<N;i++)
+          rho_array[i]=pow(rho_array[i],n);
+      pres_array=;
+      dr=;
+      density_array=;
+      pres_cof=;
+      vel_cof=;
     }
     }
