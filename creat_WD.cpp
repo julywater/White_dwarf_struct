4a02879034dfea64feaeccbf706e44b1d3af9b4e
Yike Tang <tangyike455665@gmail.com>
2014-11-26 15:26:04 -0500

    Create creat_WD.cpp

---
+++ creat_WD.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+class white_dwarf{
+  private:
+    int N; //number of bins
+    rho_array;
+    pres_array;
+    const double Radius;
+    const double Mass;
+    // inital physical consts radius and total mass
+    double length_cof;
+    double density_cof;
+    double dr;
+  public:
+    
