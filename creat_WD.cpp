d19705a9efe1a771cf863a916218b3fc37e59c8d
Yike Tang <tangyike455665@gmail.com>
2014-11-26 15:31:47 -0500

    Update creat_WD.cpp

--- creat_WD.cpp
+++ creat_WD.cpp
@@ -10,6 +10,16 @@ class white_dwarf{
     // inital physical consts radius and total mass
     double length_cof;
     double density_cof;
+    double pres_cof;
+    double vel_cof;
     double dr;
   public:
-    
+    white_dwarf(double n=1.0/3,Radius=,Mass=){
+     rho_array=;
+     pres_array=;
+     dr=;
+     density_array=;
+     pres_cof=;
+     vel_cof=;
+    }
+    }
